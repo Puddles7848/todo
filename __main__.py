2b13f8a6d8fcc4ad2c022bb8558426411c1d87f8
Puddles7848 <puddles7848@gmail.com>
2026-09-14 06:29:56 +1000

    I made ruff fix it (╥﹏╥)

--- __main__.py
+++ __main__.py
@@ -8,35 +8,33 @@ DATAFILE = None
 
 if os.name == "posix":
     if os.getenv("XDG_DATA_HOME"):
-        DATAFILE = Path(os.getenv("XDG_DATA_HOME")) / "todo.json" # pyright: ignore [reportArgumentType]
+        DATAFILE = Path(os.getenv("XDG_DATA_HOME")) / "todo.json"  # pyright: ignore [reportArgumentType]
     else:
         DATAFILE = Path.home() / ".local" / "share" / "todo.json"
 elif os.name == "nt":
     if os.getenv("APPDATA"):
-        DATAFILE = Path(os.getenv("APPDATA")) / "todo.json" # pyright: ignore [reportArgumentType]
+        DATAFILE = Path(os.getenv("APPDATA")) / "todo.json"  # pyright: ignore [reportArgumentType]
     else:
         raise OSError("%APPDATA% is not set???")
 else:
-    raise NotImplementedError("why is your os not posix or nt what are you even using (╥﹏╥)")
+    raise NotImplementedError(
+        "why is your os not posix or nt what are you even using (╥﹏╥)"
+    )
 
 # Make sure it exists
 if not DATAFILE.exists():
     DATAFILE.touch()
 
-fd = DATAFILE.open("rt+")
+fd = None
 
 # Default data
-default_data = {
-    "config": {
-        "maxlen": 16
-    },
-    "data": []
-}
+default_data = {"config": {"maxlen": 16}, "data": []}
 
 # Load data :3
 try:
     json.load(fd)
 except json.JSONDecodeError:
-    fd.seek(0);fd.truncate()
+    fd.seek(0)
+    fd.truncate()
     json.dump(default_data, fd)
     fd.flush()
